1d32215ea0c5cf6cd6a198c070398fd9ad38f92d
ottobrown <henrynlyman@gmail.com>
2022-08-28 19:46:19 -0600

    ui: add BoardUi widget to draw lines

---
+++ ui/src/board.rs
@@ -0,0 +1,95 @@
+use eframe::egui;
+use egui::{
+    Ui,
+    Response,
+    Color32,
+};
+
+use mb_goban::Board;
+
+#[derive(Clone, Copy)]
+pub struct BoardStyle {
+    /// As a fraction of the whole board size
+    pub padding: f32,
+    /// In egui screen units
+    pub line_thickness: f32,
+    pub background_color: Color32,
+}
+impl Default for BoardStyle {
+    fn default() -> Self {
+        BoardStyle {
+            padding: 0.05,
+            line_thickness: 3.0,
+            background_color: Color32::from_rgb(0xDE, 0xB8, 0x87),
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct BoardUi {
+    style: BoardStyle,
+
+    size: egui::Vec2,
+    board: Board,
+}
+impl BoardUi {
+    pub fn new(style: BoardStyle, size: egui::Vec2, board: &Board) -> Self {
+        Self {
+            style: style,
+            size: size,
+            board: board.clone(),
+        }
+    }
+}
+
+impl egui::Widget for BoardUi {
+    fn ui(self, ui: &mut Ui) -> Response {
+        let (response, painter) = ui.allocate_painter(
+            self.size,
+            egui::Sense::drag(),
+        );
+
+        // draw background color
+        painter.rect_filled(
+            egui::Rect::EVERYTHING,
+            0.0,
+            self.style.background_color,
+        );
+
+        let padding = self.size * self.style.padding;
+
+        let (w, h) = self.board.size();
+
+        let inner_w = self.size.x - 2.0*padding.x;
+        let distance_x = inner_w / ((w-1) as f32);
+
+        // draw vertical lines
+        for x in 0..w {
+            let x_pos = response.rect.min.x + padding.x + (x as f32) * distance_x;
+            let top = response.rect.min.y + padding.y;
+            let bottom = response.rect.min.y + self.size.y - padding.y;
+
+            painter.line_segment(
+                [egui::pos2(x_pos, top), egui::pos2(x_pos, bottom)],
+                egui::Stroke::new(self.style.line_thickness, Color32::BLACK),
+            );
+        }
+
+        let inner_h = self.size.y - 2.0*padding.y;
+        let distance_y = inner_h / ((h-1) as f32);
+
+        // draw horizontal lines
+        for y in 0..h {
+            let y_pos = response.rect.min.y + padding.y + (y as f32) * distance_y;
+            let left = response.rect.min.x + padding.x;
+            let right = response.rect.min.x + self.size.x - padding.x;
+
+            painter.line_segment(
+                [egui::pos2(left, y_pos), egui::pos2(right, y_pos)],
+                egui::Stroke::new(self.style.line_thickness, Color32::BLACK),
+            );
+        }
+
+        return response;
+    }
+}
--- ui/src/main.rs
+++ ui/src/main.rs
@@ -2,20 +2,39 @@ use eframe::egui;
 use eframe::App;
 use eframe::NativeOptions;
 
+use mb_goban::Board;
+
+mod board;
+
+use board::{BoardUi, BoardStyle};
+
 fn main() {
     let ops = NativeOptions::default();
 
     eframe::run_native("MBaduk", ops, Box::new(|cc| Box::new(State::new(cc))));
 }
 
-struct State {}
+struct State {
+    board: Board,
+    style: BoardStyle,
+}
 
 impl State {
     fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        Self {}
+        let board = Board::empty(19, 19);
+        let style = BoardStyle::default();
+
+        Self {
+            board: board,
+            style: style,
+        }
     }
 }
 
 impl App for State {
-    fn update(&mut self, _ctx: &egui::Context, _frame: &mut eframe::Frame) {}
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.add(BoardUi::new(self.style, egui::vec2(800.0, 800.0), &self.board))
+        });
+    }
 }
