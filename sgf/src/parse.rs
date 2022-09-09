debe8b428c7871f0e41ce0a449d6155b1466909c
ottobrown <henrynlyman@gmail.com>
2022-09-09 01:07:29 -0600

    add pest dependency and impl fmt for sgf types

--- sgf/src/parse.rs
+++ sgf/src/parse.rs
@@ -1,8 +1,24 @@
 use crate::tree::{SgfProp, SgfNode, SgfTree};
 
-pub enum ParseError {}
+use pest::Parser;
+use pest_derive::Parser;
+
+pub enum ParseError {
+    Pest(pest::error::Error<Rule>),
+}
+
+impl From<pest::error::Error<Rule>> for ParseError {
+    fn from(e: pest::error::Error<Rule>) -> Self {
+        Self::Pest(e)
+    }
+}
+
 pub type ParseResult<T> = std::result::Result<T, ParseError>;
 
-pub fn parse(input: String) -> ParseResult<SgfTree> {
+#[derive(Parser)]
+#[grammar = "sgf.pest"]
+struct SgfParser;
+
+pub fn parse(input: &str) -> ParseResult<SgfTree> {
     todo!()
 }
--- sgf/src/tree.rs
+++ sgf/src/tree.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 // TODO: optimize size of SgfProp, SgfNode, and SgfTree
 
 pub struct SgfProp {
@@ -5,11 +7,85 @@ pub struct SgfProp {
     pub(crate) values: Vec<String>,
 }
 
+impl fmt::Display for SgfProp {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.id)?;
+
+        for v in &self.values {
+            write!(f, "[{}]", v)?;
+        }
+
+        Ok(())
+    }
+}
+
 pub struct SgfNode {
     pub(crate) props: Vec<SgfProp>,
+}
+
+impl fmt::Display for SgfNode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, ";")?;
+
+        for p in &self.props {
+            write!(f, "{p}")?;
+        }
+
+        Ok(())
+    }
 }
 
 pub struct SgfTree {
     pub(crate) nodes: Vec<SgfNode>,
     pub(crate) children: Vec<SgfTree>,
+}
+impl fmt::Display for SgfTree {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "(")?;
+
+        for n in &self.nodes {
+            write!(f, "{n}")?;
+        }
+
+        for c in &self.children {
+            write!(f, "{c}")?;
+        }
+
+        write!(f, ")")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn format_prop() {
+        let prop = SgfProp {
+            id: String::from("AB"),
+            values: vec![String::from("cd"), String::from("ef")],
+        };
+
+        assert_eq!(format!("{}", prop), String::from("AB[cd][ef]"));
+    }
+
+    #[test]
+    fn format_node() {
+        let node = SgfNode {
+            props: vec![
+                SgfProp {
+                    id: String::from("AB"),
+                    values: vec![String::from("cd"), String::from("ef")],
+                },
+                SgfProp {
+                    id: String::from("AW"),
+                    values: vec![String::from("aa"), String::from("bb")],
+                },
+            ],
+        };
+
+        assert_eq!(format!("{}", node), String::from(";AB[cd][ef]AW[aa][bb]"));
+    }
+
+    // TODO: test for format tree
 }
